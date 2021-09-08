d798bd47b7e8844a3853bd36f80706691b65f447
Slim Ben Hadj <slim.benhadj@sofrecom.com>
2021-09-08 15:08:23 +0100

    Rewrite mobile phone component from scratch HAJ-510

--- shj-applicant-portal-web-fe/src/app/pages/settings/settings.component.ts
+++ shj-applicant-portal-web-fe/src/app/pages/settings/settings.component.ts
@@ -1,5 +1,5 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
-import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {ModalDismissReasons, NgbModal, NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
 import {ApplicantRitualLite} from "@model/applicant-ritual-lite.model";
 import {AuthenticationService, CardService, UserService} from "@core/services";
 import {Lookup} from "@model/lookup.model";
@@ -11,16 +11,20 @@ import {DccValidators} from "@shared/validators";
 import {UserContacts} from "@model/UserContacts.model";
 import {Router} from "@angular/router";
 import {User} from "@shared/model";
-import {CountryISO} from 'ngx-intl-tel-input';
 import {I18nService} from "@dcc-commons-ng/services";
+import {merge, Observable, Subject} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
   closeResult = '';
+
+  SAUDI_MOBILE_NUMBER_REGEX: RegExp = new RegExp("^(009665|9665|\\+9665|05|5)([0-9]{8})$");
 
   seasons: number [] = [];
   applicantRituals: ApplicantRitualLite [] = [];
@@ -35,8 +39,18 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
   originalMobileNo: any;
   originalCountryCode: any;
   userContacts: UserContacts = new UserContacts();
-  selectedCountryCode = "SA";
-  formattedCountryDial: any;
+  countries: any;
+  selectedCountryCode;
+  selectedCountryPrefix: any;
+
+  @ViewChild('instance')
+  instance: NgbTypeahead;
+
+  @ViewChild('elem')
+  elem: ElementRef;
+
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
 
   constructor(private modalService: NgbModal,
               private userService: UserService,
@@ -47,6 +61,7 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
               private translate: TranslateService,
               private authenticationService: AuthenticationService,
               private lookupsService: LookupService,
+              private elRef: ElementRef,
               private router: Router) {
   }
 
@@ -69,9 +84,10 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.loadLookups();
+
     this.userService.find(this.authenticationService.currentUser?.id).subscribe(data => {
       if (data && data.id) {
-
         this.contactsForm.disable();
         this.contactsForm.controls['email'].setValue(data.email);
         this.contactsForm.controls['mobileNumber'].setValue(data.mobileNumber);
@@ -79,6 +95,7 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
         this.originalEmail = data.email;
         this.originalCountryCode = data.countryCode.toLowerCase();
         this.selectedCountryCode = data.countryCode.toLowerCase();
+        this.selectedCountryPrefix = '+' + data.countryPhonePrefix;
       } else {
         this.toastr.error(this.translate.instant('general.route_item_not_found', {itemId: this.authenticationService.currentUser.id}),
           this.translate.instant('general.dialog_error_title'));
@@ -95,14 +112,6 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
 
     this.getRitualSeason();
     this.createForm();
-  }
-
-  public onFocus(e: Event): void {
-    e.stopPropagation();
-    setTimeout(() => {
-      const inputEvent: Event = new Event('input');
-      e.target.dispatchEvent(inputEvent);
-    }, 0);
   }
 
   get currentLanguage(): string {
@@ -133,7 +142,7 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
   private createForm() {
 
     this.contactsForm = this.formBuilder.group({
-      mobileNumber: ['', Validators.required],
+      mobileNumber: ['', [Validators.required, Validators.minLength(5)]],
       email: ['', [DccValidators.email, Validators.required]]
     });
 
@@ -182,6 +191,13 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
     return this.lookupsService;
   }
 
+  loadLookups() {
+    this.cardService.findCountries().subscribe(result => {
+      result.forEach(c => c.countryPhonePrefix = '+' + c.countryPhonePrefix);
+      this.countries = result;
+    });
+  }
+
   // convenience getter for easy access to form fields
   get f() {
     return this.contactsForm?.controls;
@@ -216,13 +232,6 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
 
   setContactsEnabled() {
     this.contactsForm.enable();
-
-  }
-
-  getPreferredCountries(): Array<any> {
-    const preferredCountries = [CountryISO.SaudiArabia, this.originalCountryCode];
-    const uniqueCountrySet = new Set(preferredCountries);
-    return [...uniqueCountrySet];
   }
 
   onSubmit() {
@@ -238,21 +247,20 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
       console.log(this.contactsForm.controls['mobileNumber'].errors);
       return;
     }
-    console.log(this.originalMobileNo != this.contactsForm.controls['mobileNumber'].value.number.replace(/\s/g, ""))
-    if (this.originalMobileNo != this.contactsForm.controls['mobileNumber'].value.number.replace(/\s/g, "") ||
+    console.log(this.originalMobileNo != this.contactsForm.controls['mobileNumber'].value.replace(/\s/g, ""))
+    if (this.originalMobileNo != this.contactsForm.controls['mobileNumber'].value.replace(/\s/g, "") ||
       this.originalEmail != this.contactsForm.controls['email'].value ||
-      this.originalCountryCode.toUpperCase() != this.contactsForm.controls['mobileNumber'].value.countryCode.toUpperCase()) {
+      this.originalCountryCode.toUpperCase() != this.selectedCountryCode.toUpperCase()) {
 
 
       let reg1 = / /g;
       let reg2 = /\+/gi;
       let reg3 = /\-/gi;
 
-      this.userContacts.countryCode = this.contactsForm.controls['mobileNumber'].value.countryCode.toUpperCase();
+      this.userContacts.countryCode = this.selectedCountryCode.toUpperCase();
       this.userContacts.email = this.contactsForm.controls['email'].value;
-      this.userContacts.mobileNumber = this.contactsForm.controls['mobileNumber'].value.number.replace(reg1, "").replace(reg3, "");
-      this.formattedCountryDial = this.contactsForm.controls['mobileNumber'].value.dialCode.replace(reg2, "");
-      this.userContacts.countryPhonePrefix = this.formattedCountryDial;
+      this.userContacts.mobileNumber = this.contactsForm.controls['mobileNumber'].value.replace(reg1, "").replace(reg3, "");
+      this.userContacts.countryPhonePrefix = this.selectedCountryPrefix.replace(reg2, "");
 
       console.log(this.userContacts);
 
@@ -285,6 +293,32 @@ export class SettingsComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngOnDestroy() {
     document.querySelector('body').classList.remove('settings');
+  }
+
+
+  public openTypeahead(): void {
+    // Dispatch event on input element that NgbTypeahead is bound to
+    this.elem.nativeElement.dispatchEvent(new Event('input'));
+    // Ensure input has focus so the user can start typing
+    this.elem.nativeElement.focus();
+  }
+
+  search = (text$: Observable<string>) => {
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+    return merge(text$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? this.countries.filter(c => c.lang.toLowerCase() === this.i18nService.language.substr(0, 2))
+        : this.countries.filter(c => c.lang.toLowerCase() === this.i18nService.language.substr(0, 2)).filter(v => v.label.toLowerCase().indexOf(term.toLowerCase()) > -1 || v.countryPhonePrefix.toLowerCase().indexOf(term) > -1
+        ))));
+  }
+
+  inputFormatter = (x: { countryPhonePrefix: string }) => x.countryPhonePrefix
+
+  onSelect($event, input) {
+    $event.preventDefault();
+    this.selectedCountryPrefix = $event.item.countryPhonePrefix;
+    this.selectedCountryCode = $event.item.code.toLowerCase();
+    input.value = '';
   }
 
 }
--- shj-applicant-portal-web-fe/src/app/pages/settings/settings.module.ts
+++ shj-applicant-portal-web-fe/src/app/pages/settings/settings.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SettingsRoutingModule } from './settings-routing.module';
-import { SettingsComponent } from './settings.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SettingsRoutingModule} from './settings-routing.module';
+import {SettingsComponent} from './settings.component';
 import {TranslateModule} from "@ngx-translate/core";
 import {SharedModule} from "@shared/shared.module";
+import {NgSelectModule} from "@ng-select/ng-select";
+import {NgbDropdownModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   imports: [
     CommonModule,
     SettingsRoutingModule,
     TranslateModule,
+    NgSelectModule,
+    NgbDropdownModule,
     SharedModule
   ],
   declarations: [SettingsComponent]
 })
-export class SettingsModule { }
+export class SettingsModule {
+}
