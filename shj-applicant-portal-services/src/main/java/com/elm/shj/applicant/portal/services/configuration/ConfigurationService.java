73a927dbfd75eeef657d2028e509ff6ee2fe14b3
salzoubi <salzoubi@elm.sa>
2021-11-02 16:08:56 +0300

    HAJ-780 As an Applicant portal I want to expose an API for mobile app configuration (key, value)

---
+++ shj-applicant-portal-services/src/main/java/com/elm/shj/applicant/portal/services/configuration/ConfigurationService.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2021 ELM. All rights reserved.
+ */
+package com.elm.shj.applicant.portal.services.configuration;
+
+import com.elm.shj.applicant.portal.services.dto.ConfigDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Service handling configuration
+ *
+ * @author salzoubi
+ * @since 1.1.0
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class ConfigurationService {
+
+    @Value("${location.request.in.minutes}")
+    private int locationRequestInMinutes;
+
+    public List<ConfigDto> findUserLocationRequestDuration() {
+        List<ConfigDto> configList =new ArrayList<>();
+        ConfigDto config = new ConfigDto();
+        config.setConfKey("location.request.in.minutes");
+        config.setConfValue(String.valueOf(locationRequestInMinutes));
+        configList.add(config);
+
+        return configList;
+    }
+}
