9213092574540d2a773451875756a1503edf9439
Slim Ben Hadj <slim.benhadj@sofrecom.com>
2021-11-22 08:59:23 +0100

    Add copyright and class comments HAJ-1

--- shj-applicant-portal-web/src/main/java/com/elm/shj/applicant/portal/web/notification/EUserNotificationType.java
+++ shj-applicant-portal-web/src/main/java/com/elm/shj/applicant/portal/web/notification/EUserNotificationType.java
@@ -1,5 +1,14 @@
+/*
+ *  Copyright (c) 2021 ELM. All rights reserved.
+ */
 package com.elm.shj.applicant.portal.web.notification;
 
+/**
+ * Enum for user notification type
+ *
+ * @author Slim Ben Hadj
+ * @since 1.1.0
+ */
 public enum EUserNotificationType {
     ALL,
     GENERAL,
