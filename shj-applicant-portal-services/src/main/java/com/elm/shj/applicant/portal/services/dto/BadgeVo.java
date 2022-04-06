8951ec758df002e3921aa1615fcac15dbeaaa952
salzoubi <salzoubi@elm.sa>
2022-04-06 11:20:14 +0300

    HAJ-1771 get applicant card image

--- shj-applicant-portal-services/src/main/java/com/elm/shj/applicant/portal/services/dto/BadgeVo.java
+++ shj-applicant-portal-services/src/main/java/com/elm/shj/applicant/portal/services/dto/BadgeVo.java
@@ -3,19 +3,22 @@
  */
 package com.elm.shj.applicant.portal.services.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Badge value object.
  *
- * @author salzoubi
- * @since 1.0.0
+ * @author ahmad flaifel
+ * @since 1.1.0
  */
 @Getter
 @Setter
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
-public class BadgeVo {
+@NoArgsConstructor
+public class BadgeVO {
+
     private String badgeImage;
 }
