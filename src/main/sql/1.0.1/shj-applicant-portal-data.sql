093e813a2c9bded077f6ed1ed2c4d3c8ce4b629a
aflaifel <aflaifel@elm.sa>
2022-06-16 12:14:25 +0300

    update scripts HAJ-1

--- src/main/sql/1.0.1/shj-applicant-portal-data.sql
+++ src/main/sql/1.0.1/shj-applicant-portal-data.sql
@@ -1,21 +1,8 @@
 USE sha_portal
 GO
 
-INSERT INTO sha_portal.sha_config (conf_key, conf_value)
-VALUES ('sms.api.token', 'D60A13C6105742AD99FEDD608632A157');
-INSERT INTO sha_portal.sha_config (conf_key, conf_value)
-VALUES ('sms.api.url', 'https://172.16.72.65/api/SmartCards/smsService');
+UPDATE sha_portal.sha_config SET conf_key = 'huic.sms.api.token' WHERE conf_key = 'sms.api.token';
+UPDATE sha_portal.sha_config SET conf_key = 'huic.sms.api.url' WHERE conf_key = 'sms.api.url';
 
-GO
-
-UPDATE sha_portal.sha_config
-SET conf_key = 'huic.sms.api.token'
-WHERE conf_key = 'sms.api.token';
-
-UPDATE sha_portal.sha_config
-SET conf_key = 'huic.sms.api.url'
-WHERE conf_key = 'sms.api.url';
-
-INSERT INTO sha_portal.sha_config(conf_key, conf_value)
-VALUES('huic.sms.api.mock.enabled','true');
+INSERT INTO sha_portal.sha_config(conf_key, conf_value) VALUES('huic.sms.api.mock.enabled','true');
 GO
