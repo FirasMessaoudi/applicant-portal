d95d2d9a6f9b686f1bc3067fe92b4dd3fa71df9d
Noor Nawaz <nnawaz@elm.sa>
2022-06-16 12:02:30 +0300

    HAJ-1 HUIC sms service mock added

--- src/main/sql/1.0.1/shj-applicant-portal-data.sql
+++ src/main/sql/1.0.1/shj-applicant-portal-data.sql
@@ -7,3 +7,15 @@ INSERT INTO sha_portal.sha_config (conf_key, conf_value)
 VALUES ('sms.api.url', 'https://172.16.72.65/api/SmartCards/smsService');
 
 GO
+
+UPDATE sha_portal.sha_config
+SET conf_key = 'huic.sms.api.token'
+WHERE conf_key = 'sms.api.token';
+
+UPDATE sha_portal.sha_config
+SET conf_key = 'huic.sms.api.url'
+WHERE conf_key = 'sms.api.url';
+
+INSERT INTO sha_portal.sha_config(conf_key, conf_value)
+VALUES('huic.sms.api.mock.enabled','true');
+GO
