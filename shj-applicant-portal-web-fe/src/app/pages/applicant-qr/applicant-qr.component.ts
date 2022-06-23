9a148e164be7b41ced50b09018af5e069963d026
adhaoui <adhaoui@elm.sa>
2022-06-23 14:52:42 +0300

    HAJ-583 updating pilgrim and staff qr pages

--- shj-applicant-portal-web-fe/src/app/pages/applicant-qr/applicant-qr.component.ts
+++ shj-applicant-portal-web-fe/src/app/pages/applicant-qr/applicant-qr.component.ts
@@ -13,6 +13,17 @@ export class ApplicantQrComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    let ua = navigator.userAgent.toLowerCase();
+    let isAndroid = ua.indexOf("android") > -1 && ua.indexOf("mobile") > -1;
+    let isiPhone = ua.indexOf("iphone") > -1 && ua.indexOf("mobile") > -1;
+    let isiPad = ua.indexOf("ipad") > -1 && ua.indexOf("mobile") > -1;
+    if (isAndroid) {
+      window.open('https://play.google.com/store/apps/details?id=com.elm.shaaer.applicant.mobile', '_blank');
+    } else if (isiPhone || isiPad) {
+      window.open('https://apps.apple.com/ng/app/pilgrimage-app-pilgrims/id1626714163', '_blank');
+    } else {
+      document.getElementsByTagName('html') [0].remove();
+    }
   }
 
 }
--- shj-applicant-portal-web-fe/src/app/pages/staff-qr/staff-qr.component.ts
+++ shj-applicant-portal-web-fe/src/app/pages/staff-qr/staff-qr.component.ts
@@ -13,6 +13,20 @@ export class StaffQrComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    let ua = navigator.userAgent.toLowerCase();
+    let isAndroid = ua.indexOf("android") > -1 && ua.indexOf("mobile") > -1;
+    let isiPhone = ua.indexOf("iphone") > -1 && ua.indexOf("mobile") > -1;
+    let isiPad = ua.indexOf("ipad") > -1 && ua.indexOf("mobile") > -1;
+    if (isAndroid) {
+      // TODO put staff url
+      //window.open('https://play.google.com/store/apps/details?id=com.elm.shaaer.staff.mobile', '_blank');
+    } else if (isiPhone || isiPad) {
+      // TODO put staff url
+      //window.open('https://apps.apple.com/ng/app/pilgrimage-app-pilgrims/idXXXXXXXXXX', '_blank');
+    } else {
+      document.getElementsByTagName('html') [0].remove();
+    }
   }
+
 
 }
