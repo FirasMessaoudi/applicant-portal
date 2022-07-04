96f467d2588c3cbe1d4662fb892d9328a390f22e
aflaifel <aflaifel@elm.sa>
2022-07-04 23:22:48 +0300

    Release 1.2.6-SNAPSHOT HAJ-1

--- src/main/sql/1.2.4/shj-applicant-portal-data.sql
+++ src/main/sql/1.2.4/shj-applicant-portal-data.sql
@@ -7,11 +7,3 @@ INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('mohu.emergency
 GO
 INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('holy.mosque.emergency.phone.number', '1966');
 GO
-INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('ethrai.url', 'https://ethrai.com/en/');
-GO
-INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('adahi.url', 'https://www.adahi.org/');
-GO
-INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('awareness.url', 'https://guide.haj.gov.sa');
-GO
-INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('fatwas.phone.number', '8002451000');
-GO
---
+++ src/main/sql/1.2.5/shj-applicant-portal-data.sql
@@ -0,0 +1,10 @@
+USE sha_portal
+GO
+INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('ethrai.url', 'https://ethrai.com/en/');
+GO
+INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('adahi.url', 'https://www.adahi.org/');
+GO
+INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('awareness.url', 'https://guide.haj.gov.sa');
+GO
+INSERT INTO sha_portal.sha_config (conf_key, conf_value) VALUES ('fatwas.phone.number', '8002451000');
+GO
